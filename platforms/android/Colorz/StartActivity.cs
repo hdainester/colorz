b9881f3356a279493446793b387597a24c92238c
hdainester <27729552+hdainester@users.noreply.github.com>
2019-06-11 16:41:52 +0200

    Included vs project setup for android

---
+++ platforms/android/Colorz/StartActivity.cs
@@ -0,0 +1,24 @@
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+
+namespace Chaotx.Colorz.Android {
+    [Activity(Label = "Colorz"
+        , MainLauncher = true
+        , Icon = "@drawable/icon"
+        , Theme = "@style/Theme.Splash"
+        , AlwaysRetainTaskState = true
+        , LaunchMode = LaunchMode.SingleInstance
+        , ScreenOrientation = ScreenOrientation.FullUser
+        , ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.Keyboard | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize | ConfigChanges.ScreenLayout)]
+    public class StartActivity : Microsoft.Xna.Framework.AndroidGameActivity {
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+            var g = new ColorzGame();
+            SetContentView((View)g.Services.GetService(typeof(View)));
+            g.Run();
+        }
+    }
+}
+
